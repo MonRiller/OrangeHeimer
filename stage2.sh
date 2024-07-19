60c88b386f33fa7b42cd606a79c643155f4528cc
Jeremy Murray <45187326+Lapras@users.noreply.github.com>
2024-07-19 09:24:21 -0500

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -10,8 +10,8 @@ sshpass -p 0rangel4nd ssh missileadmin@localhost "echo 0rangel4nd | sudo -S serv
 sleep 1
 
 # Set up port forward going from thrower port 4200 to controller port 8080 IP format -- localhost:local_up:controller_ip:controller_up
-sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -M -S ~/.ssh/masterSock -L 127.0.0.1:4200:172.31.86.120:8080
+sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -M -S ~/.ssh/masterSock -L 0.0.0.0:4200:172.31.86.120:8080
 # Set up port forward going from SSH server 4242 to thrower port 4201 IP format -- relay_ip_from_controller:controller_down:localhost:local_down
-sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -S ~/.ssh/masterSock -R 172.100.0.1:4242:127.0.0.1:4201
+sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -S ~/.ssh/masterSock -R 0.0.0.0:4242:127.0.0.1:4201
 # Set up port forward going from SSH server 4243 to thrower port 4202 IP format -- relay_ip_from_controller:controller_down:localhost:local_down
-sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -S ~/.ssh/masterSock -R 172.100.0.1:4243:127.0.0.1:4202
+sshpass -p 0rangel4nd ssh -Nf missileadmin@localhost -S ~/.ssh/masterSock -R 0.0.0.0:4243:127.0.0.1:4202
